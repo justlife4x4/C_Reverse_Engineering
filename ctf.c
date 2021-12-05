3387ef05870b383854fefd078736ea4533793007
justlife4x4 <59994040+justlife4x4@users.noreply.github.com>
2021-12-05 18:05:21 +0530

    Create ctf.c

---
+++ ctf.c
@@ -0,0 +1,90 @@
+
+/******************************************************************************
+                               Made by dark_haxor for fun purpose!
+                                 Contact: derpkilla3@gmail.com
+                               C Reverse ENgineering CTF for my frenz
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int pass=7832,passing_value,input_value;
+    printf("[*]Welcome to Raven Silo Launch site, Kursk, Soviet Union[*]\n");
+    
+    printf("                             \n");
+printf("                             \n");
+printf("========================================| \n");
+printf("========================================| \n");
+printf("++++++++++++ ===== ^=======+++++++++++++| \n");
+printf("            +     / \\     +            |\n");
+printf("            +    /***\\    +            |\n");
+printf("            +   /. . .\\   +            |\n");
+printf("            +   |+++++|    +            |\n");
+printf("            +   |. . .|    +            |\n");
+printf("            +   |. . .|    +            |\n");
+printf("            +   |. . .|    +            |\n");
+printf("            +   |. . .|    +            |\n");
+printf("            +   |. . .|    +            |\n");
+printf("            +   |. . .|    +            |\n");
+printf("            +   |. . .|    +            |\n");
+printf("            +   |. . .|    +            |\n");
+printf("            +  /|. . .|\\  +            |\n");
+printf("            + / |. . .| \\ +            |\n");
+printf("            +/  |. . .|  \\+            |\n");
+printf("            +|  |. . .|  | +            |\n");
+printf("            +|  |. . .|  | +            |\n");
+printf("            +|  |=====|  |  +           |\n");
+printf("            +| /      \\ |              |\n");
+printf("            +|/        \\|              |\n");
+printf("========================================|\n");
+printf("========================================|\n");
+sleep(1);
+printf("[*]Target is set to 0x556e69746564205374617465732c2057617368696e67746f6e204443\n");
+printf("=============================================================================\n");
+sleep(1);
+printf("\nEnter the correct password to launch the communists ICBM\n");
+sleep(1);
+printf("Enter your code: ");
+scanf("%d",&input_value);
+//passing the password to messenger
+passing_value=input_value;
+//Checking with Kremlin's communist database to verify
+
+if(passing_value==pass) {
+    sleep(2);
+printf(" Code Acepted! \n");
+sleep(2);
+printf("Opening Silo Door in");
+printf("============================================= \n");
+ sleep(2);
+ printf("Missile away! \n");
+ printf("============================================= \n");
+ sleep(4);
+printf("                             \n");
+printf("         (_ ._  _ , _ ._      \n");
+printf("        (_ ' ( `  )_  .__)    \n");
+printf("      ( (  (    )   `)  ) _)  \n");
+printf("     (__ (_   (_ . _) _) ,__) \n");
+printf("         `~~`; ' . /`~~`      \n");
+printf("              ;   ;           \n");
+printf("              /    ;          \n");
+printf("_____________/_ __ _;_________\n");
+ sleep(3);
+  printf("============================================= \n");
+  printf("Congrats! The red star won!\n");
+  sleep(2);
+  printf("You have succefully killed the capitalists! \n");
+
+}
+else {
+printf("============================================= \n");
+printf("False code! Traitor! \n");
+sleep(1);
+printf("You execution date is tommorow! \n");
+sleep(1);
+printf("Meet us in the Red Square by 0900 hrs\n ");
+}
+
+    
+}
